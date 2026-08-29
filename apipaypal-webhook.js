f5c508cef23775d748bed175f2026f7c893a2eb2
mohammedawny59-beep <mohammedawny59@gmail.com>
2026-08-29 03:28:22 +0300

    Delete apipaypal-webhook.js

--- apipaypal-webhook.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(200).json({ status: "QuickTiny PayPal webhook ready" });
-  }
-
-  console.log("PayPal webhook:", req.body);
-
-  return res.status(200).json({ received: true });
-}
